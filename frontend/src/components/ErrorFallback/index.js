e8be1fdd377ca24efb8d2b6650af29afc115c59e
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-16 16:39:59 -0300

    fix: Typo in prop

--- frontend/src/components/ErrorFallback/index.js
+++ frontend/src/components/ErrorFallback/index.js
@@ -5,7 +5,7 @@ const ErrorFallback = () => {
     <div className='error-fallback'>
       <h2>Ooops, something went wrong :(</h2>
       <a href={window.location.origin}>
-        <span class='material-icons'>refresh</span>
+        <span className='material-icons'>refresh</span>
       </a>
     </div>
   );
