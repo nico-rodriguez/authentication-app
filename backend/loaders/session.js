e7dee5aeab51ac68a8d7fb95a86d48ad84a00279
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-17 14:49:44 -0300

    fix: Set sameSite cookie attribute to false

--- backend/loaders/session.js
+++ backend/loaders/session.js
@@ -28,7 +28,7 @@ module.exports = session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: false,
     secure: config.NODE_ENV === 'production',
   },
   store: sessionStore,
