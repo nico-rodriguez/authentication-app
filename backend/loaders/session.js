171556b400db79032d91832830591af6a1bde616
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-16 23:39:58 -0300

    feat: Use Redis for session store in production

--- backend/loaders/session.js
+++ backend/loaders/session.js
@@ -2,36 +2,25 @@ const session = require('express-session');
 const config = require('../config');
 
 // Configuration of the store for the session of express-session
-let sessionStore;
-if (config.NODE_ENV === 'development') {
-  // Redis configuration and connection (only in development)
-  const RedisStore = require('connect-redis')(session);
-  const { createClient } = require('redis');
-  const redisClient = createClient({ legacyMode: true });
-  redisClient
-    .connect()
-    .then(() => {
-      console.log('Connected to session database!');
-    })
-    .catch((err) => {
-      console.error('Could not connect to session database.');
-      console.error(err);
-      process.exit(1);
-    });
-  sessionStore = new RedisStore({
-    client: redisClient,
-    host: config.REDIS_HOST,
-    port: config.REDIS_PORT,
+// Redis configuration and connection (only in development)
+const RedisStore = require('connect-redis')(session);
+const { createClient } = require('redis');
+const redisClient = createClient({ legacyMode: true });
+redisClient
+  .connect()
+  .then(() => {
+    console.log('Connected to session database!');
+  })
+  .catch((err) => {
+    console.error('Could not connect to session database.');
+    console.error(err);
+    process.exit(1);
   });
-} else if (config.NODE_ENV === 'production') {
-  const MemoryStore = require('memorystore')(session);
-  // MemoryStore configuration
-  sessionStore = new MemoryStore({
-    checkPeriod: 1000 * 60 * 60 * 24, // prune expired entries every 24h
-  });
-} else {
-  console.error(`Unhandled environment variable $NODE_ENV=${config.NODE_ENV}`);
-}
+const sessionStore = new RedisStore({
+  client: redisClient,
+  host: config.REDIS_HOST,
+  port: config.REDIS_PORT,
+});
 
 module.exports = session({
   name: 'sessionId',
