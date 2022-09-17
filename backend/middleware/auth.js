575332bbc9bfa456e3ddb35cdf0f98904dfab20b
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-17 17:01:13 -0300

    refactor: Return from middleware if user is unauthorized

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const config = require('../config');
 
 function isUserAuth(err, req, res, next) {
   if (!req.user) {
-    res.redirect(401, `${config.FRONTEND_URL}/login`);
+    return res.redirect(401, `${config.FRONTEND_URL}/login`);
   }
 
   next();
