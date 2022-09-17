217355ab8ba23a3b063772aab18b4dca344c99e0
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-17 19:41:40 -0300

    feat: Use mongodb-memory-server in development

--- backend/loaders/mongoose.js
+++ backend/loaders/mongoose.js
@@ -1,14 +1,33 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 
-// Connect to database
-mongoose
-  .connect(config.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to users database!');
-  })
-  .catch((err) => {
-    console.error('Could not connect to users database.');
-    console.error(err);
-    process.exit(1);
+if (config.NODE_ENV === 'development') {
+  const { MongoMemoryServer } = require('mongodb-memory-server');
+
+  MongoMemoryServer.create().then((mongodb) => {
+    const uri = mongodb.getUri();
+
+    mongoose
+      .connect(uri)
+      .then(() => {
+        console.log('Connected to users database!');
+      })
+      .catch((err) => {
+        console.error('Could not connect to users database.');
+        console.error(err);
+        process.exit(1);
+      });
   });
+} else if (config.NODE_ENV === 'production') {
+  // Connect to database
+  mongoose
+    .connect(config.MONGODB_URL)
+    .then(() => {
+      console.log('Connected to users database!');
+    })
+    .catch((err) => {
+      console.error('Could not connect to users database.');
+      console.error(err);
+      process.exit(1);
+    });
+}
