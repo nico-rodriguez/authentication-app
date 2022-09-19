e05da2993608e17f308ba4f8955b175cb37d238f
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-18 23:34:09 -0300

    fix: Clear storage when reloading from error page

--- frontend/src/components/ErrorFallback/index.js
+++ frontend/src/components/ErrorFallback/index.js
@@ -1,10 +1,13 @@
+import storage from 'utils/storage';
 import './ErrorFallback.css';
 
 const ErrorFallback = () => {
   return (
     <div className='error-fallback'>
       <h2>Ooops, something went wrong :(</h2>
-      <a href={window.location.origin}>&#xe5d5;</a>
+      <a onClick={() => storage.clear()} href={window.location.origin}>
+        &#xe5d5;
+      </a>
     </div>
   );
 };
