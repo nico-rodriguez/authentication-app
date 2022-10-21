0ec4b36aa7291dfbc5bd52ff698c27c324462f7c
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-10-21 20:42:22 -0300

    refactor: Catch "Invalid file type" as a MulterError

--- backend/middleware/multerErrors.js
+++ backend/middleware/multerErrors.js
@@ -4,9 +4,6 @@ function multerErrorHandler(err, req, res, next) {
   if (err instanceof MulterError) {
     res.status(400);
     return res.json(err.message);
-  } else if (err.message.includes('Invalid file type')) {
-    res.status(400);
-    return res.json(err.message);
   }
 
   next(err);
