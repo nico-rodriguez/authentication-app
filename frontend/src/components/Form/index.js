82fb8d0ffa304d482f900ed3594383e5cb3ebcd5
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-10-22 00:21:47 -0300

    feat: Disable form while signing/logging

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -5,38 +5,46 @@ import './Form.css';
 
 import constants from 'constants/index';
 import { useUser } from 'hooks/useUser';
+import { useState } from 'react';
 
 export function Form({ buttonText, handleFormData }) {
   const { setAuthenticating } = useUser();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsSubmitting(true);
 
     const email = event.target.email.value;
     const password = event.target.password.value;
 
     await handleFormData(email, password);
+    setIsSubmitting(false);
   };
 
   const handleOAuth = () => {
-    setAuthenticating(true);
+    if (!isSubmitting) {
+      setAuthenticating(true);
+    }
   };
 
   return (
     <form className='form' onSubmit={handleSubmit}>
       <input
+        disabled={isSubmitting}
         className='form__email-input'
         type='email'
         placeholder='&#xe158; Email'
         name='email'
       />
       <input
+        disabled={isSubmitting}
         className='form__password-input'
         type='password'
         placeholder='&#xe88d; Password'
         name='password'
       />
-      <button className='form__button' type='submit'>
+      <button className='form__button' type='submit' disabled={isSubmitting}>
         {buttonText}
       </button>
       <div className='form__alternatives'>
@@ -47,14 +55,18 @@ export function Form({ buttonText, handleFormData }) {
           <a
             title='Google'
             onClick={handleOAuth}
-            href={`${constants.BACKEND_URL}/api/v1/auth/google`}
+            href={
+              isSubmitting ? '#' : `${constants.BACKEND_URL}/api/v1/auth/google`
+            }
           >
             <img width='43' height='43' src={googleLogo} alt='Google logo' />
           </a>
           <a
             title='GitHub'
             onClick={handleOAuth}
-            href={`${constants.BACKEND_URL}/api/v1/auth/github`}
+            href={
+              isSubmitting ? '#' : `${constants.BACKEND_URL}/api/v1/auth/github`
+            }
           >
             <img width='43' height='43' src={githubLogo} alt='Github logo' />
           </a>
